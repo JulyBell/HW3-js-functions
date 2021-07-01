c9da425ba3e499797cac1c6c810a454a5360d374
JulyBell <julybell16@gmail.com>
2021-07-01 21:24:06 +0300

    added amendments

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 console.log('------------- # 4');
 
 function plus(num1, num2){
-	console.log('Plus: ', num1 + num2);
+	return num1 + num2;
 }
 
 function minus(num1, num2){
-	console.log('Minus: ', num1 - num2);
+	return num1 - num2;
 }
 
 function multiply(num1, num2){
-	console.log('Multiply: ', num1 * num2);
+	return num1 * num2;
 }
 
 function divide (num1, num2){
-	console.log('Divide', num1 / num2);
+	return num1 / num2;
 }
 
-plus(9, 6);
-minus(17, 6);
-multiply(3, 6);
-divide(33, 3);
+console.log(plus(9, 6));
+console.log(minus(17, 6));
+console.log (multiply(3, 6));
+console.log(divide(33, 3));
 
 
 console.log('------------- # 5');
@@ -46,37 +46,33 @@ console.log('------------- # 7');
 
 function pow(x, n){
 	let result = Math.pow(x, n);
-	console.log(result);
+	return result;
 }
 
-pow(5, 4);
+console.log(pow(5, 4));
 
 console.log('------------- # 8');
 
 function isBigger(num1, num2){
-	if(num1 > num2){
-		console.log(true);
-	}else{
-		console.log(false);
-	}
-}
+	let comp = (num1 > num2) ? true : false;
+	return comp;
 
-isBigger(13, 5);
-isBigger(6, 45);
+	}
+
+
+console.log(isBigger(13, 5));
+console.log(isBigger(6, 45));
 
 
 console.log('------------- # 9');
 
 function isSmaller(num1, num2){
-	if(num1 < num2){
-		console.log(true);
-	}else{
-		console.log(false);
-	}
+	let comp = (num1 < num2) ? true : false
+	return comp;
 }
 
-isSmaller(34, 45);
-isSmaller(22, 12);
+console.log(isSmaller(34, 45));
+console.log(isSmaller(22, 12));
 
 
 
